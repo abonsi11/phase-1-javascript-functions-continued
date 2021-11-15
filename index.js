46e725779aa7a5bb84cc79ff9e39add2c8eaf572
Anthony Bonsi <xbonsi96@gmail.com>
2021-11-14 17:09:07 -0700

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-function saturdayFun() {
-    return 'This Saturday, I want to roller-skate!'
+function saturdayFun(activity = 'roller-skate') {
+  return `This Saturday, I want to ${activity}!`
 }
-saturdayFun();
+saturdayFun('bathe my dog')
 
-
+function mondayWork(task = 'go to the office') {
+    return `This Monday, I will ${task}.`
+}
+mondayWork('work from home')
 
